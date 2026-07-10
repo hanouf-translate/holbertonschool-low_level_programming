1f48652ad2c35a3cecd6333ad317f6889e314feb
hanouf-translate <Alhanouftranslation@outlook.com>
2026-07-10 13:21:23 +0000

    a function that prints the 9 times table, starting with 0.

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Description: Uses nested loops and strict spacing logic to print
+ * a formatted 10x10 multiplication grid up to 9 * 9.
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
+
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
