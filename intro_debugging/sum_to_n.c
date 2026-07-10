4e9cefc15cc25c6f18d1fcef8a06fdea6f8b7179
hanouf-translate <Alhanouftranslation@outlook.com>
2026-07-10 13:36:12 +0000

    Learn to trace a simple loop and verify a function’s return value.

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
+/**
+ * sum_to_n - Computes the sum of all integers from 1 to n
+ * @n: The stopping number
+ *
+ * Return: The total sum
+ */
 int sum_to_n(int n)
 {
-    int i;
-    int sum = 0;
+	int i;
+	int sum = 0;
 
-    for (i = 1; i < n; i++)
-        sum += i;
+	/* Changed loop condition from 'i < n' to 'i <= n' to include n */
+	for (i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
 
-    return n;
+	/* Changed from 'return n;' to return the actual computed sum */
+	return (sum);
 }
 
 /* -- DO NOT Modify the code below this line -- */
-
 int main(void)
 {
-    printf("%d\n", sum_to_n(10));
-    return 0;
+	printf("%d\n", sum_to_n(10));
+	return (0);
 }
