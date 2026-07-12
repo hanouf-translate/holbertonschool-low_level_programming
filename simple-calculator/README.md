3986419bd3af7943d51e938e870067553482793d
hanouf-translate <Alhanouftranslation@outlook.com>
2026-07-12 08:25:00 +0000

    simple calculator

---
+++ simple-calculator/README.md
@@ -0,0 +1,10 @@
+# Simple Calculator
+By completing this project, you will be able to:
+
+Design a small but complete C program from requirements.
+Build a menu-driven application using loops and conditionals.
+Structure code using functions to avoid duplication.
+Implement arithmetic operations using basic language operators.
+Read user input with scanf.
+Handle important edge cases such as division by zero.
+Produce C code that compiles with strict compiler settings and no warnings.
---
+++ simple-calculator/calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	bool isend = true;
+	int choice;
+	printf("welcome\n");
+	
+	while(isend){
+	 printf("Simple Calculator\n"
+        "1) Add\n"
+        "2) Subtract\n"
+        "3) Multiply\n"
+        "4) Divide\n"
+        "0) Quit\n");
+
+	 scanf("%d", &choice);
+
+	 if (choice ==1){
+		 /* code goes here*/
+	 }
+	 else if (choice ==2) {
+		 /* code goes here*/}
+
+	 else if (choice == 3) {
+		 /* code goes here*/}
+	else if (choice == 4){
+		/* code goes here*/}
+	else if (choice == 0){
+		isend = false;}
+
+
+		
+	}
+	printf("Bye!");
+	return 0;
+
+	}
