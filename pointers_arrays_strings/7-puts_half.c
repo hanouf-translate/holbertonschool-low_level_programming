617546d02f28c0dc3508c2a849f225d8d4a77501
hanouf-translate <hanouf-translate>
2026-07-15 15:06:38 +0000

    a function that prints half of a string, followed by a new line.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
 /**
- * puts_half - Prints the second half of a string, followed by a new line
- * @str: The pointer to the string
- *
- * Return: void
- */
+ * puts_half - Prints the second half of a string, followed by a new line
+ * @str: The pointer to the string
+ *
+ * Return: void
+ */
 void puts_half(char *str)
 {
 	int len = 0;
-	int start;
 	int i;
 
-	/* Find the total length of the string */
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	/* Apply the formula to find the starting index */
-	start = len - ((len + 1) / 2);
 
-	/* Print from the starting index to the end of the string */
-	for (i = start; i < len; i++)
+	for (i = (len + 1) / 2; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
