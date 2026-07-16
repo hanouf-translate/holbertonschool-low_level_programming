789d6d47eb8f35232fc2f85e5890039b920fbe92
hanouf-translate <hanouf-translate>
2026-07-16 09:41:50 +0000

    a function that copies a string.

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string up to n bytes
+ * @dest: The destination buffer to copy the string to
+ * @src: The source string to copy
+ * @n: The maximum number of bytes to copy from src
+ *
+ * Return: A pointer to the destination buffer dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
