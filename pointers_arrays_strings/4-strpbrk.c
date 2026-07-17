e6db71f7f52fe081ad38335f4873cc40858f3d3d
hanouf-translate <hanouf-translate>
2026-07-17 11:26:04 +0000

    a function that searches a string for any of a set of bytes.

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: The string to search
+ * @accept: The string containing bytes to look for
+ *
+ * Return: A pointer to the matching byte in s,
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+
+	return (NULL);
+}
