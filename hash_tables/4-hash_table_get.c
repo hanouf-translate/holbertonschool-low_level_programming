a6abde2b699c52a070fa066a013c9cafe72be786
hanouf-translate <hanouf-translate>
2026-08-15 18:56:02 +0300

    function that retrieves a value associated with a key.

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: Pointer to the hash table to look into.
+ * @key: The key string to look for.
+ *
+ * Return: The value associated with the element,
+ *         or NULL if the key couldn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
